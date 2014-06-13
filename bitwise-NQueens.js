2cc6c6195c0bd15f2ab6931fc22c36a0672a1e0d
Hack Reactor Students <accounts@hackreactor.com>
2014-06-12 20:53:12 -0700

    completed bitwise n-queens

--- bitwise-NQueens.js
+++ bitwise-NQueens.js
@@ -1,32 +1,23 @@
 var countNQueens = function(n){
   var all = Math.pow(2,n) -1;
-  var solutionCount;
+  var solutionCount = 0;
 
   var findSolutions = function(cols,ld,rd){
 
-    var pos = ~(cols || ld || rd) & all;
+    var pos = ~(cols | ld | rd) & all;
+
     while(pos>0){
-      //get the bit
       var bit = -pos & pos;
-      //XOR ing the bit, which removes the bit from the position
       pos = pos ^ bit;
 
-      //add the bit to the column, leftDiagnoal and rightDiagonal
-      cols = cols | bit;
-      ld = ld | bit;
-      rd = rd | bit;
-
-    //calling recursively and passing in the new state
-    //which would mean shifting the diagonals as we walk over
-      findSolutions(cols,ld << 1, rd >> 1);
+      findSolutions((cols | bit), (ld | bit) << 1, (rd | bit) >> 1);
 
     }
     if (cols === all) {
       solutionCount++;
     }
-
   };
-
+  findSolutions(0, 0, 0);
   console.log(solutionCount);
   return solutionCount;
 };
